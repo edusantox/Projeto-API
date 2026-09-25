562408ca20901d4cbc3b86193f65c4c035d99fb1
edusantox <05eduardorodrigues@gmail.com>
2026-09-25 16:17:40 -0300

    Alteração

--- js/api.js
+++ js/api.js
@@ -1,13 +1,13 @@
 const api = {
-    async buscarPensamentos() {
-    try {
-        const response = await fetch('http://localhost:3000/pensamentos')
-        return await response.json()
+    buscarPensamentos() {
+        try {
+            const response = fetch('http://localhost:3000/pensamentos')
+            return response.json()
     }
     catch {
         alert('Erro ao buscar pensamentos')
         throw error
-    }
+        }
     }
 }
 
